16d8798439694272ea785b42c2ee5577ea4a6d4a
SchneiderPeter12 <psc4909@students.fhv.at>
2024-03-11 16:04:50 +0100

    changes

---
+++ src/test/java/com/example/demo/DemoUnitTest.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DemoUnitTest {
+
+
+        @Test
+        public void testConcatenate() {
+            MyUnit myUnit = new MyUnit();
+
+            String result = myUnit.concatenate("one", "two");
+
+            assertEquals("onetwo", result);
+
+        }
+
+}
